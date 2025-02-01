f8403cb663e4231c9ab7d6b386c68af86541bf0d
SantiagoPQX <santiago@pricerqx.com>
2025-02-01 14:25:44 -0500

    init

---
+++ thryve/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      <div className="flex w-full justify-center items-center text-3xl font-bold">THRYVE</div>
+    </div>
+  );
+}
+
+export default App;
