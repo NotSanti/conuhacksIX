515ce5dc4a02d70434c19692a533d1b8539db89a
SantiagoPQX <santiago@pricerqx.com>
2025-02-01 14:44:43 -0500

    lib setups

--- thryve/src/App.tsx
+++ thryve/src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 
 import "./App.css";
+import { Button } from "@headlessui/react";
 
 function App() {
   return (
     <div className="w-full h-full">
-      <div className="flex w-full justify-center items-center text-3xl font-bold">THRYVE</div>
+      <div className="flex w-full justify-center items-center text-3xl font-bold">
+        THRYVE
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <Button
+          className={
+            "border border-red-500 hover:cursor-pointer bg-amber-500 rounded p-2 active:bg-amber-700"
+          }
+          onClick={() => {}}
+        >
+          Headless UI Button
+        </Button>
+      </div>
     </div>
   );
 }
