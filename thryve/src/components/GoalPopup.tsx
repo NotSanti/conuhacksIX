d3beabb856e1467f3d0584f036e56cafd680a8a6
amina0914 <aminaturdalieva@gmail.com>
2025-02-01 23:14:18 -0500

    Added footsteps

--- thryve/src/components/GoalPopup.tsx
+++ thryve/src/components/GoalPopup.tsx
@@ -5,16 +5,27 @@ import { RiFootprintLine } from "react-icons/ri";
 interface GoalProps {
     goalTitle?: string;
     goaldDescription?: string;
-    goaldSteps?: string;
+    goalSteps: number;
+    goalStepsLeft: number;
 }
 
-function GoalPopup({goalTitle} : GoalProps){
+function GoalPopup({goalTitle, goalSteps, goalStepsLeft} : GoalProps){
     return (
         <div className="text-lg font-bold text-black">
             {goalTitle}
-            <RiFootprintFill className="size-16 ..."/>
-            <RiFootprintLine className="size-16 ..."/>
-        </div>)
+            <div className="flex flex-wrap">
+                {goalSteps > 0 &&
+                    Array.from({ length: goalSteps }).map((_, index) => (
+                    <RiFootprintFill key={index} className="size-6 mx-0.5" />
+                ))}
+
+                {goalStepsLeft > 0 &&
+                    Array.from({ length: goalStepsLeft }).map((_, index) => (
+                    <RiFootprintLine key={index} className="size-6 mx-0.5" />
+                ))}
+            </div>
+          </div>
+        )
 }
 
 export default GoalPopup;
